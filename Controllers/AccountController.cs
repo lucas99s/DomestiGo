974a4e0a5d9d43c670035184e2ff57eab64c424c
Lucas Rodrigues <lucasdeveloper99s@gmail.com>
2023-09-12 21:19:19 -0300

    first commit

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DomestiGo.Models;
+
+namespace DomestiGo.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View("Login");
+        }
+
+        public IActionResult SignIn()
+        {
+            return View("SignIn");
+        }
+    }
+}
