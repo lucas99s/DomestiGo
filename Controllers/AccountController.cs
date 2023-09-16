a000ab6d4ab6092357a144fab3595935b031fb82
Lucas Rodrigues <lucasdeveloper99s@gmail.com>
2023-09-16 12:36:16 -0300

    sqlite configuration

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -23,5 +23,37 @@ namespace DomestiGo.Controllers
         {
             return View("SignIn");
         }
+
+        [HttpPost]
+        public IActionResult SignIn(UserModel user)
+        {
+            using(var db = new DomestiGoContext())
+            {
+                db.Add(user);
+                db.SaveChanges();
+            }
+
+            return View("SignIn");
+        }
+
+        [HttpPost]
+        public IActionResult Login(UserModel user)
+        {
+            using (var db = new DomestiGoContext())
+            {
+                var existingUser = db.Users.FirstOrDefault(u => u.Email == user.Email);
+
+                if (existingUser != null)
+                {
+                    var password = db.Users.FirstOrDefault(u => u.Password == user.Password);
+                    if(password != null)
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+            }
+            return View();
+        }
+
     }
 }
